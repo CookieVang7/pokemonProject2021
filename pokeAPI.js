37ce07c83287f85db7da1015e65a8c1e2b578bac
Cole <cole.n.maxwell99@gmail.com>
2021-06-29 16:01:03 -0500

    Changed readablity of getPokemonInfo

--- pokeAPI.js
+++ pokeAPI.js
@@ -3,11 +3,11 @@ Fetch data in JSON format from PokeAPI about a
 pokemon using either an numeric id or
 string that is a valid pokemon name
 */
-const getPokemonInfo = async id => {
+async function getPokemonInfo(id) {
     const pokeURL = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const result = await fetch(pokeURL);
-    const pokemon = await result.json();
-    console.log(pokemon);
+    const pokemonData = await result.json();
+    console.log(pokemonData);
 }
 
 //Run in browser for result
