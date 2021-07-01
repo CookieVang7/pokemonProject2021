3c8d2b45a439ae7615351506d52ae47704d8a7cd
Cole <cole.n.maxwell99@gmail.com>
2021-07-01 16:42:39 -0500

    Add img, name, and id to createPokemonCard

--- pokeAPI.js
+++ pokeAPI.js
@@ -2,21 +2,21 @@ const originalPokemonLastIndex = 151
 /* 
 Fetch data in JSON format from pokeapi.co about a 
 pokemon using either an numeric id or
-string that is a valid pokemon name
+string that is a valid pokemon name and 
+displays card
 */
-async function getPokemonInfo(id) {
+async function getPokemon(id) {
     const pokeURL = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const result = await fetch(pokeURL);
     const pokemonData = await result.json();
-    console.log(pokemonData);
+    //console.log(pokemonData);
+    createPokemonCard(pokemonData);
 }
-//Test
-//getPokemonInfo(1);
 
 // Fetches the original 151 pokemon (with happen to be 1:151)
 async function fetchOriginalPokemon() {
     for (let index = 1; index <= originalPokemonLastIndex; index++) {
-        await getPokemonInfo(index);
+        await getPokemon(index);
     }
 }
 fetchOriginalPokemon();
@@ -43,13 +43,29 @@ const pokeCardContainer = document.getElementById('pokeCardContainer');
 /* Creates an html div for on each call that displays a pokemon
 */
 function createPokemonCard(pokemon) {
+    //An element to add cards to
     const pokemonElemenmt = document.createElement('div');
-    pokemonElemenmt.classList.add('pokemon');
+    //Add a class to the div for css
+    pokemonElemenmt.classList.add('pokemonCard');
 
-    const pokeInnerHTML = "test";
+    //Variables for card data
+    const pokeName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    //const pokeTypes = pokemon.types.map(typeData => typeData.type.name)
+    //Inner HTML for each Pokemon card
+    const pokeInnerHTML = `
+    <div class="info">
+        <h3 class="name">${pokeName}</h3>
+        <span class="number"> ${pokemon.id} </span>
+    </div>
+
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+    </div>
+
+
+    `;
 
     pokemonElemenmt.innerHTML = pokeInnerHTML
-    
     pokeCardContainer.appendChild(pokemonElemenmt)
 }
-createPokemonCard();
+//createPokemonCard();
