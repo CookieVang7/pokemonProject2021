f10eeff37c2e2af8840e3736727c03b4d59ceeff
Cookie Vang <vang2660@morris.umn.edu>
2021-07-07 22:13:30 -0500

    Add methods to grab pokemon types and move info

--- pokeBattle.js
+++ pokeBattle.js
@@ -8,23 +8,20 @@ async function getSinglePokemonData(id){
     const pokeURL = `https://pokeapi.co/api/v2/pokemon/${id}`; //getting the info from the api
     const result = await fetch(pokeURL); //the raw data from the api
     const pokeData = await result.json(); //the data now in json form
-    const pokeStats = await pokeData.stats;
+    const pokeStats = await pokeData.stats; //returned as [hp,attack,defense,special attack,special defense,speed]
 
-    //pokeStats is an array with 6 elements: hp, attack, defense, special attack, special defense, and speed
-
+    const pokName = pokeData.name[0].toUpperCase() + pokeData.name.slice(1);
     const pokHP = getPokemonHP(pokeStats);
     const pokAttack = getPokemonAttack(pokeStats);
     const pokDefense = getPokemonDefense(pokeStats);
     const pokSpeed = getPokemonSpeed(pokeStats);
 
-    console.log(pokAttack,pokDefense,pokHP,pokSpeed);
-    //return pokHP,pokAttack,pokDefense,pokSpeed;
+    return [pokName,pokHP,pokAttack,pokDefense,pokSpeed]; //the values returned as an array of elements
 }
 
 function getPokemonHP(pokeStats){
     const hp = pokeStats[0].base_stat; 
     return hp;
-    //console.log(hp);
 }
 
 function getPokemonAttack(pokeStats){
@@ -33,7 +30,6 @@ function getPokemonAttack(pokeStats){
     const finalAttack = (attack + specAttack)/2;
     const attackCeiling = Math.ceil(finalAttack);
     return attackCeiling;
-    //console.log(attackCeiling);
 }
 
 function getPokemonDefense(pokeStats){
@@ -42,19 +38,76 @@ function getPokemonDefense(pokeStats){
     const finalDefense = (defense + specDefense)/2;
     const defenseCeiling = Math.ceil(finalDefense);
     return defenseCeiling;
-    //console.log(defenseCeiling);
 }
 
 function getPokemonSpeed(pokeStats){
     const speed = pokeStats[5].base_stat; 
     return speed;
-    //console.log(speed);
+}
+
+async function getPokemonTypes(id){ //gets the type(s) of a pokemon
+    const pokeURL = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const result = await fetch(pokeURL); 
+    const pokeData = await result.json();
+    const numberOfTypes = pokeData.types.length; //the number of types a pokemon has
+
+    const type1 = pokeData.types[0].type.name;
+    let type2 = 'none'; //using 'let' instead of 'const' since let can change
+    if (numberOfTypes > 1){ //accounting for if a pokemon has 2 types instead of just 1
+        type2 = pokeData.types[1].type.name;
+    }
+
+    return [type1,type2];
 }
 
 /*
-Function used for damage calculation. The formula is lifted from RhyQuinn on : https://www.youtube.com/watch?v=OMA7hY48jG8
+Functions used for damage calculation. The formula is lifted from RhyQuinn on : https://www.youtube.com/watch?v=OMA7hY48jG8
 */
 
-async function damageCalculator(userPokemonId, cpuPokemonId){
+async function getMove(id){ //gets a move's name, type, power and accuracy
+    const moveURL = `https://pokeapi.co/api/v2/move/${id}`;
+    const result = await fetch(moveURL);
+    const moveData = await result.json();
 
+    const name = moveData.name[0].toUpperCase() + moveData.name.slice(1);
+    const power = moveData.power;
+    const accuracy = moveData.accuracy;
+    const moveType = moveData.type.name;
+    
+    return [name,moveType,power,accuracy];
+}
+
+/*
+Assumptions: 
+-all pokemon are the same level of 35 (for now)
+*/
+async function damageCalculator(attackerId, defenderId, moveId){
+    const attackerPokemonStats = getSinglePokemonData(attackerId); //returned as [name,hp,attack,defense,speed]
+    const attackNum = attackerPokemonStats[2];
+    const attackerTypes = getPokemonTypes(attackerId); 
+    
+    
+    const defenderPokemonStats = getSinglePokemonData(defenderId); 
+    const defenseNum = defenderPokemonStats[3];
+    const defenderTypes = getPokemonTypes(defenderId);
+
+    console.log(attackerPokemonStats);
+    console.log(defenderPokemonStats);
+    console.log(attackerTypes);
+    console.log(defenderTypes);
+    console.log(getMove(moveId));
+    //status update: able to get stats, types, and move info at this point
+}
+
+async function pokemonThatGoesFirst(userPokemonId, cpuPokemonId){ //determines which pokemon is faster
+    const userPokemonSpeed = getSinglePokemonData(userPokemonId)[4];
+    const cpuPokemonSpeed = getSinglePokemonData(cpuPokemonId)[4];
+
+    if (userPokemonSpeed >= cpuPokemonSpeed){
+        damageCalculator(userPokemonId,cpuPokemonId);
+    }
+    
+    else {
+        damageCalculator(cpuPokemonId,userPokemonId);
+    }
 }
